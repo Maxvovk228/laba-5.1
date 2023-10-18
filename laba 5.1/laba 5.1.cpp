0be38cf15070ab517fd4bd276816a16bb08798a7
Maxvovk228 <145869483+Maxvovk228@users.noreply.github.com>
2023-10-18 21:55:25 +0300

    1 commit

---
+++ laba 5.1/laba 5.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double a, const double b); // прототип
+int main()
+{
+	double s, t;
+		cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double c = (h( s , t ) * h( s , t) * h( s , t) * h( s, t) + h (1,s*s + t*t))/(1 + h (s*t ,1) * h(s * t, 1));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double a, const double b) // визначення
+{
+	return (a * 1.0 / (b * b + 1) + 1.0 / (a * a + b * b));
+}
